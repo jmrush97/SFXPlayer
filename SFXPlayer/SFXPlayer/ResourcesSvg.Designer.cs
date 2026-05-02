24d5f4e8e44aa74b9135421184313ed4a51a51f7
john.rush@benden.co.uk <john.rush@benden.co.uk>
2026-05-02 20:50:44 +0100

    update after run

--- SFXPlayer/SFXPlayer/ResourcesSvg.Designer.cs
+++ SFXPlayer/SFXPlayer/ResourcesSvg.Designer.cs
@@ -22,7 +22,7 @@ namespace SFXPlayer {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class ResourcesSvg {
+    public class ResourcesSvg {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace SFXPlayer {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SFXPlayer.ResourcesSvg", typeof(ResourcesSvg).Assembly);
@@ -51,21 +51,12 @@ namespace SFXPlayer {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to .
-        /// </summary>
-        internal static string headphones_svg {
-            get {
-                return ResourceManager.GetString("headphones.svg", resourceCulture);
             }
         }
     }
